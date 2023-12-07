1983d788b8fd40dcd573f2c36583d4a9b9263972
Jerry Melton <jerryleemelton@gmail.com>
2023-12-06 21:05:14 -0800

    Fix bug with removing portfolio tabs

--- src/components/PortfolioCreative.jsx
+++ src/components/PortfolioCreative.jsx
@@ -34,7 +34,7 @@ const Portfolio = () => {
               <div className="title_flex">
                 <div className="left">
                   {/* <span>Portfolio</span> */}
-                  <h3>Creative Portfolio</h3>
+                  <h3>My Work</h3>
                 </div>
               </div>
             </div>
@@ -42,13 +42,13 @@ const Portfolio = () => {
 
             <div className="portfolio_filter">
               <Tabs>
-                {/* <TabList>
+                <TabList>
                   <Tab>All</Tab>
                   <Tab>Vimeo</Tab>
                   <Tab>Youtube</Tab>
                   <Tab>Photography</Tab>
                   <Tab>Details</Tab>
-                </TabList> */}
+                </TabList>
                 {/* END TABLIST */}
                 <div className="list_wrapper">
                   <TabPanel>
@@ -627,6 +627,132 @@ const Portfolio = () => {
       {/* END MODAL FOR PORTFOLIO DETAILS */}
 
       {/* START MODAL FOR PORTFOLIO DETAILS */}
+      <Modal
+        isOpen={isOpen4}
+        onRequestClose={toggleModalFour}
+        contentLabel="My dialog"
+        className="mymodal"
+        overlayClassName="myoverlay"
+        closeTimeoutMS={500}
+      >
+        <div className="jlm_modalbox_news portfolio_tm_modalbox">
+          <button className="close-modal" onClick={toggleModalFour}>
+            <img src="assets/img/svg/cancel.svg" alt="close icon" />
+          </button>
+          {/* END CLOSE ICON */}
+          <div className="box_inner">
+            <div className="description_wrap scrollable">
+              <div className="image">
+                <img src="assets/img/thumbs/4-3.jpg" alt="tumb" />
+                <div
+                  className="main"
+                  style={{
+                    backgroundImage: "url(assets/img/portfolio/7.jpg)",
+                  }}
+                ></div>
+              </div>
+              {/* END IMAGE */}
+              <div className="portfolio_main_title">
+                <h3>Ave Simone</h3>
+                <span>Details</span>
+              </div>
+              {/* END portfolio_main_title */}
+              <div className="main_details">
+                <div className="textbox">
+                  <p>
+                    We live in a world where we need to move quickly and iterate
+                    on our ideas as flexibly as possible. Building mockups
+                    strikes the ideal balance between true-life representation
+                    of the end product and ease of modification.
+                  </p>
+                  <p>
+                    Mockups are useful both for the creative phase of the
+                    project - for instance when you're trying to figure out your
+                    user flows or the proper visual hierarchy - and the
+                    production phase when they will represent the target
+                    product. Making mockups a part of your creative and
+                    development process allows you to quickly and easily ideate.
+                  </p>
+                </div>
+                <div className="detailbox">
+                  <ul>
+                    <li>
+                      <span className="first">Client</span>
+                      <span>Alvaro Morata</span>
+                    </li>
+                    <li>
+                      <span className="first">Category</span>
+                      <span>Details</span>
+                    </li>
+                    <li>
+                      <span className="first">Date</span>
+                      <span>March 07, 2021</span>
+                    </li>
+                    <li>
+                      <span className="first">Share</span>
+                      <Social />
+                      {/* END SOCIAL SHARE */}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              {/* main_details */}
+
+              <div className="additional_images">
+                <ul className="gallery_zoom">
+                  <li>
+                    <div className="list_inner">
+                      <div className="my_image">
+                        <img src="img/thumbs/4-2.jpg" alt="thumb" />
+                        <div
+                          className="main"
+                          style={{
+                            backgroundImage: "url(assets/img/portfolio/6.jpg)",
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                  </li>
+                  {/* END SHOT */}
+                  <li>
+                    <div className="list_inner">
+                      <div className="my_image">
+                        <img src="img/thumbs/4-2.jpg" alt="thumb" />
+                        <div
+                          className="main"
+                          style={{
+                            backgroundImage: "url(assets/img/portfolio/5.jpg)",
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                  </li>
+                  {/* END SHOT */}
+                  <li>
+                    <div className="list_inner">
+                      <div className="my_image">
+                        <img src="img/thumbs/4-2.jpg" alt="thumb" />
+                        <div
+                          className="main"
+                          style={{
+                            backgroundImage: "url(assets/img/portfolio/4.jpg)",
+                          }}
+                        ></div>
+                      </div>
+                    </div>
+                  </li>
+                  {/* END SHOT */}
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* END BOX INNER */}
+        </div>
+        {/* END MODALBOX NEWS */}
+      </Modal>
+      {/* END MODAL FOR PORTFOLIO DETAILS */}
+
+      {/* START PORTFOLIO DETAIL MODAL 1 ============================================================================ */}
       <Modal
         isOpen={isOpen4}
         onRequestClose={toggleModalFour}
