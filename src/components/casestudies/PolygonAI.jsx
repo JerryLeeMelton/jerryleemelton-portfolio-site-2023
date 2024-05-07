679ee7c6c33a59b8f2a2ef88634be1c9c5dece66
Jerry Melton <jerryleemelton@gmail.com>
2024-05-07 07:51:27 -0700

    Text update

--- src/components/casestudies/PolygonAI.jsx
+++ src/components/casestudies/PolygonAI.jsx
@@ -15,15 +15,38 @@ export const PolygonAI = () => {
               <h3>Polygon AI</h3>
               <h4>Challenge:</h4>
               <p>
-                Finding other musicians who share interests can be very
-                challenging and subject to happenstance.
+                Pipsqueak Pro, now known as Polygon AI, offered powerful but
+                very specific and technical capabilities that were hampered by
+                an unfocused user interface design.
               </p>
               <h4>Solution: </h4>
               <p>
-                Music Link Up is a an app that makes it simple for musicians to
-                find like-minded to find each other and start playing together.
+                A rebranding and interface redesign that clarifies UI sections
+                and provides a more focused and cohesive user experience.
               </p>
               <h4>Original State</h4>
+              <p>
+                Originally, Pipsqueak Pro users were presented with three panels
+                with various functions nested in each.; There was no clear flow
+                for users to take when working on a project and features were
+                not grouped together in intuitive ways. Additionally, the Rewire
+                team had no designer prior to my joining the company so the
+                visual styling was unfocused which further contributed to user
+                confusion.
+              </p>
+              <h4>Redesign Process</h4>
+              <p>
+                The name Polygon AI was chosen by the team to better reflect the
+                capabilities of the software, which centers around annotating
+                images with shapes referred to as regions of interest (R.O.I.s),
+                as well as the company's forward-thinking, AI-focused business.
+              </p>
+              <p>
+                As the company's newly hired designer, I had the opportunity to
+                lead the redesign process, which began with the application's
+                logo.
+              </p>
+              <h5>Logo Design</h5>
               <p>Polygon OG Image</p>
               <p>Polygon OG Image</p>
               <h4>Redesign Process</h4>
