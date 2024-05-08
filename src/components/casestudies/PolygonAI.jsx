7203b9630581946804bdb94f613906b063f9858e
Jerry Melton <jerryleemelton@gmail.com>
2024-05-07 17:03:16 -0700

    Text updates

--- src/components/casestudies/PolygonAI.jsx
+++ src/components/casestudies/PolygonAI.jsx
@@ -13,11 +13,12 @@ export const PolygonAI = () => {
           <div className="title_flex">
             <div className="left">
               <h3>Polygon AI</h3>
+              <p>***Hero Image***</p>
               <h4>Challenge:</h4>
               <p>
                 Pipsqueak Pro, now known as Polygon AI, offered powerful but
-                very specific and technical capabilities that were hampered by
-                an unfocused user interface design.
+                very technical and niche capabilities that were hampered by an
+                unfocused user interface design.
               </p>
               <h4>Solution: </h4>
               <p>
@@ -26,27 +27,35 @@ export const PolygonAI = () => {
               </p>
               <h4>Original State</h4>
               <p>
-                Originally, Pipsqueak Pro users were presented with three panels
-                with various functions nested in each.; There was no clear flow
-                for users to take when working on a project and features were
-                not grouped together in intuitive ways. Additionally, the Rewire
-                team had no designer prior to my joining the company so the
-                visual styling was unfocused which further contributed to user
-                confusion.
+                Originally, Pipsqueak Pro presented users with three panels
+                which had various functions nested in each. There was no clear
+                flow for users to follow when working on a project and features
+                were not grouped together in intuitive ways. Additionally, the
+                Rewire team had no designer prior to me joining the company and
+                the resulting unfocused visual styling further contributed to
+                user confusion.
               </p>
               <h4>Redesign Process</h4>
+              <h5>Name and Logo</h5>
               <p>
                 The name Polygon AI was chosen by the team to better reflect the
                 capabilities of the software, which centers around annotating
                 images with shapes referred to as regions of interest (R.O.I.s),
                 as well as the company's forward-thinking, AI-focused business.
-              </p>
-              <p>
                 As the company's newly hired designer, I had the opportunity to
                 lead the redesign process, which began with the application's
                 logo.
               </p>
-              <h5>Logo Design</h5>
+              <p>***Polygon Logo vs Pip Logo</p>
+              <p>
+                After many sketches and iterations, we decided on a logo that
+                was sleeker and more edgy than the previous Pipsqueak Pro logo.
+                The team felt that the angular design fit the cutting edge
+                aesthetic that we wanted to bring to the re-branded app. This
+                would then inform the creative direction for the interface
+                design going forward.
+              </p>
+
               <p>Polygon OG Image</p>
               <p>Polygon OG Image</p>
               <h4>Redesign Process</h4>
