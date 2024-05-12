30e9dd1d8897d7175b71a6c17f78227db826d3a0
Jerry Melton <jerryleemelton@gmail.com>
2024-05-11 19:46:41 -0700

    Text spacing adjustments

--- src/components/casestudies/PolygonAI.jsx
+++ src/components/casestudies/PolygonAI.jsx
@@ -18,19 +18,43 @@ export const PolygonAI = () => {
                 alt="Polygon Main Image"
                 class="marg-bottom-one-and-half"
               />
-              <h6>Challenge:</h6>
-              <p class="marg-bottom-one">
+              {/* <h6>Challenge:</h6> */}
+              <p class="marg-bottom-one-and-half">
+                <span
+                  className="text-bold"
+                  style={{
+                    backgroundColor: "transparent",
+                    marginBottom: "0",
+                    marginRight: "0.5rem",
+                    padding: "0",
+                  }}
+                >
+                  Challenge:
+                </span>
+                {"  "}
                 Pipsqueak Pro, now known as Polygon AI, offered powerful but
                 very technical and niche capabilities that were hampered by an
                 unfocused user interface design.
               </p>
-              <h6>Solution: </h6>
+              {/* <h6>Solution: </h6> */}
               <p class="marg-bottom-three">
-                A rebranding and interface redesign that clarifies UI sections
-                and provides a more focused and cohesive user experience.
+                <span
+                  className="text-bold background-transparent"
+                  style={{
+                    backgroundColor: "transparent",
+                    marginBottom: "0",
+                    marginRight: "0.5rem",
+                    padding: "0",
+                  }}
+                >
+                  Solution:
+                </span>
+                {"  "}A rebranding and interface redesign that clarifies UI
+                sections and provides a more focused and cohesive user
+                experience.
               </p>
-              <h4>Original State</h4>
-              <p>
+              <h4 className="marg-bottom-half">Original State</h4>
+              <p class="marg-bottom-one-and-half">
                 Originally, Pipsqueak Pro presented users with three panels
                 which had various functions nested in each. There was no clear
                 flow for users to follow when working on a project and features
@@ -39,10 +63,14 @@ export const PolygonAI = () => {
                 the resulting unfocused visual styling further contributed to
                 user confusion.
               </p>
-              <p>***Pipsqueak Pro OG Image***</p>
-              <h4>Redesign Process</h4>
-              <h6>Name and Logo</h6>
-              <p>
+              <img
+                src="assets/img/casestudies/polygon/polygon_casestudy_hero_cropped.webp"
+                alt="Polygon Main Image"
+                class="marg-bottom-three"
+              />
+              <h4 class="marg-bottom-one-and-half">Redesign Process</h4>
+              <h6 className="marg-bottom-half">Name and Logo</h6>
+              <p class="marg-bottom-one-and-half">
                 The name Polygon AI was chosen by the team to better reflect the
                 capabilities of the software, which centers around annotating
                 images with shapes referred to as regions of interest (R.O.I.s),
@@ -51,8 +79,12 @@ export const PolygonAI = () => {
                 lead the redesign process, which began with the application's
                 logo.
               </p>
-              <p>***Polygon Logo vs Pip Logo</p>
-              <p>
+              <img
+                src="assets/img/casestudies/polygon/polygon_casestudy_hero_cropped.webp"
+                alt="Polygon Main Image"
+                class="marg-bottom-one-and-half"
+              />
+              <p className="marg-bottom-three">
                 After many sketches and iterations, we decided on a logo that
                 was sleeker and more edgy than the previous Pipsqueak Pro logo.
                 The team felt that the angular design fit the cutting edge
@@ -60,11 +92,15 @@ export const PolygonAI = () => {
                 would then inform the creative direction for the interface
                 design going forward.
               </p>
-              <h6>Interface Redesign</h6>
-              <p>***Wireframes***</p>
-              <h4>Additional Features</h4>
-              <h6>Color Themes</h6>
-              <p>
+              <h6 className="marg-bottom-one-and-half">Interface Redesign</h6>
+              <img
+                src="assets/img/casestudies/polygon/polygon_casestudy_hero_cropped.webp"
+                alt="Polygon Main Image"
+                class="marg-bottom-three"
+              />
+              <h4 className="marg-bottom-one-and-half">Additional Features</h4>
+              <h6 className="marg-bottom-half">Color Themes</h6>
+              <p className="marg-bottom-one-and-half">
                 As part of the improvements to the user interface, I was also
                 responsible for adding color themes to Polygon AI. This added to
                 the polish of Polygon as well as provides users with a better
@@ -73,8 +109,8 @@ export const PolygonAI = () => {
                 Polygon in a way that was extensible, leaving open the option to
                 add fun and functional new color themes in the future.
               </p>
-              <h6>In-App Tutorials</h6>
-              <p>
+              <h6 className="marg-bottom-half">In-App Tutorials</h6>
+              <p className="marg-bottom-one-and-half">
                 As I finalized the interface improvements that I had designed,
                 the Rewire team decided that some of the app's more specialized
                 features would benefit from more direct guidance than our help
@@ -84,15 +120,19 @@ export const PolygonAI = () => {
                 usability and user retention for Polygon, which directly
                 translated into increased revenue for the company.
               </p>
-              <h6>Analytics</h6>
-              <p>
+              <h6 className="marg-bottom-half">Analytics</h6>
+              <p className="marg-bottom-three">
                 Another important feature that I created for Polygon was the
                 ability to track user analytics data. This helped the team to
                 better understand user behavior and any potential pain points in
                 the user experience.
               </p>
-              <h4>Final Product</h4>
-              <p>*** Current Polygon UI ***</p>
+              <h4 className="marg-bottom-one-and-half">Final Product</h4>
+              <img
+                src="assets/img/casestudies/polygon/polygon_casestudy_hero_cropped.webp"
+                alt="Polygon Main Image"
+                class="marg-bottom-one-and-half"
+              />
               <p>
                 I was fortunate to join Rewire relatively early in their
                 existence as a company. As such, I had the opportunity to do a
