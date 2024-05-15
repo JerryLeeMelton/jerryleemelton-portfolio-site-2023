f1bda304e5a46d8effd1ae605a88b8f0b707b4d0
Jerry Melton <jerryleemelton@gmail.com>
2024-05-15 11:54:47 -0700

    Text updates

--- src/components/casestudies/PolygonAI.jsx
+++ src/components/casestudies/PolygonAI.jsx
@@ -33,8 +33,8 @@ export const PolygonAI = () => {
                 </span>
                 {"  "}
                 Pipsqueak Pro, now known as Polygon AI, offered powerful but
-                very technical and niche capabilities that were hampered by an
-                unfocused user interface design.
+                highly technical and niche capabilities. However, its potential
+                was hampered by an unfocused user interface design.
               </p>
               {/* <h6>Solution: </h6> */}
               <p class="marg-bottom-three">
@@ -49,19 +49,16 @@ export const PolygonAI = () => {
                 >
                   Solution:
                 </span>
-                {"  "}A rebranding and interface redesign that clarifies UI
-                sections and provides a more focused and cohesive user
-                experience.
+                {"  "}A rebranding and interface redesign to clarify UI
+                sections, providing a more focused and cohesive user experience.
               </p>
               <h4 className="marg-bottom-half">Original State</h4>
               <p class="marg-bottom-one-and-half">
-                Originally, Pipsqueak Pro presented users with three panels
-                which had various functions nested in each. There was no clear
-                flow for users to follow when working on a project and features
-                were not grouped together in intuitive ways. Additionally, the
-                Rewire team had no designer prior to me joining the company and
-                the resulting unfocused visual styling further contributed to
-                user confusion.
+                Initially, Pipsqueak Pro featured three panels with various
+                nested functions, lacking a clear workflow for users. Features
+                were not intuitively grouped, and the absence of a dedicated
+                designer prior to my hiring led to unfocused visual styling,
+                further contributing to user confusion.
               </p>
               <img
                 src="assets/img/casestudies/polygon/pipsqueakpro_main_screen.webp"
@@ -71,13 +68,12 @@ export const PolygonAI = () => {
               <h4 class="marg-bottom-one-and-half">Redesign Process</h4>
               <h6 className="marg-bottom-half">Name and Logo</h6>
               <p class="marg-bottom-one-and-half">
-                The name Polygon AI was chosen by the team to better reflect the
-                capabilities of the software, which centers around annotating
-                images with shapes referred to as regions of interest (R.O.I.s),
-                as well as the company's forward-thinking, AI-focused business.
-                As the company's newly hired designer, I had the opportunity to
-                lead the redesign process, which began with the application's
-                logo.
+                The team chose the name Polygon AI to better reflect the
+                software's capabilities, which involve annotating images with
+                shapes known as regions of interest (R.O.I.s), and to align with
+                the company's AI-focused direction. As the newly hired designer,
+                I had the opportunity to lead the redesign process, which began
+                with the application's logo.
               </p>
               <img
                 src="assets/img/casestudies/polygon/pip_vs_poly_logos.webp"
@@ -85,12 +81,10 @@ export const PolygonAI = () => {
                 class="marg-bottom-one-and-half"
               />
               <p className="marg-bottom-three">
-                After many sketches and iterations, we decided on a logo that
-                was sleeker and more edgy than the previous Pipsqueak Pro logo.
-                The team felt that the angular design fit the cutting edge
-                aesthetic that we wanted to bring to the re-branded app. This
-                would then inform the creative direction for the interface
-                design going forward.
+                After numerous sketches and iterations, we settled on a sleeker,
+                edgier logo compared to the previous Pipsqueak Pro design. The
+                angular logo aligned with the cutting-edge aesthetic we aimed
+                for and set the creative direction for the interface design.
               </p>
               <h6 className="marg-bottom-one-and-half">Interface Redesign</h6>
               <img
@@ -100,39 +94,32 @@ export const PolygonAI = () => {
               />
               <p>
                 When approaching the redesign of the interface for Polygon, one
-                of the biggest challenges that we faced was the non-linear
-                nature of the user workflow. Users would often bounce around
-                from section to section and complete tasks in an order that was
-                particular to them, so it was important to us to allow for that
-                while bringing better visual organization to the application.
+                of the significant challenges was the non-linear user workflow.
+                Users frequently moved between sections, completing tasks in
+                their unique order. It was crucial to maintain this flexibility
+                while improving visual organization.
               </p>
               <p>
-                Ultimately, we decided to keep the three panel design that the
-                app current had, but we added icon-based buttons along the left
-                and right borders of the app that would serve as navigation.
-                This allowed us to create clear sections in the app while
-                keeping the layout similar for existing users.
+                We retained the three-panel design but introduced icon-based
+                buttons along the app’s left and right borders for navigation.
+                This created clear sections within the app while preserving a
+                familiar layout for existing users.
               </p>
               <h4 className="marg-bottom-one-and-half">Additional Features</h4>
               <h6 className="marg-bottom-half">Color Themes</h6>
               <p className="marg-bottom-one-and-half">
                 As part of the improvements to the user interface, I was also
-                responsible for adding color themes to Polygon AI. This added to
-                the polish of Polygon as well as provides users with a better
-                experience by allowing them to customize the appearance of the
-                app to suit their personal preference. I implemented themes into
-                Polygon in a way that was extensible, leaving open the option to
-                add fun and functional new color themes in the future.
+                responsible for adding color themes to Polygon AI. This not only
+                polished the app but also allowed users to personalize the
+                appearance according to their preferences. The implementation
+                was extensible, paving the way for additional themes in the
+                future.
               </p>
               <h6 className="marg-bottom-half">In-App Tutorials</h6>
               <p className="marg-bottom-one-and-half">
-                As I finalized the interface improvements that I had designed,
-                the Rewire team decided that some of the app's more specialized
-                features would benefit from more direct guidance than our help
-                documentation could provide. Consequently, I implemented a
-                series of guided, in-app tutorials for Polygon AI to help users
-                better understand the features of the app. This further improved
-                usability and user retention for Polygon, which directly
+                Recognizing the need for direct guidance on specialized
+                features, I developed a series of guided in-app tutorials. These
+                tutorials improved usability and user retention, which directly
                 translated into increased revenue for the company.
               </p>
               <h6 className="marg-bottom-half">Analytics</h6>
@@ -149,12 +136,10 @@ export const PolygonAI = () => {
                 class="marg-bottom-one-and-half"
               />
               <p>
-                I was fortunate to join Rewire relatively early in their
-                existence as a company. As such, I had the opportunity to do a
-                lot of very exciting and impactful work on Polygon AI. I am
-                extremely proud of what I acheived with Polygon and I am excited
-                to carry the lessons that I learned while working on it through
-                to my future projects.
+                Joining Rewire early in its development allowed me to make
+                significant and impactful contributions to Polygon AI. I am
+                extremely proud of my achievements with Polygon and am eager to
+                apply the lessons learned to future projects.
               </p>
             </div>
           </div>
